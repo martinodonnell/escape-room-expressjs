d5cc6dabc9971414b683828d81b7cac3b7e57954
Martin O'Donnell <modo1997@hotmail.co.uk>
2021-01-12 18:27:54 +0000

    Setup socket to check db for cookies on connection/emit

--- public/javascripts/client/roomService.js
+++ public/javascripts/client/roomService.js
@@ -7,6 +7,17 @@ const checkIfRoomExists = async (roomID) => {
     },
   });
 
-  const jsonResponse = await response.json();
-  return jsonResponse;
+  return await response.json();
+};
+
+const getRoomCookies = async (roomID) => {
+  const response = await fetch(`/api/room/${roomID}/cookie`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${getCookie('authToken')}`
+    },
+  });
+
+  return  await response.json();
 };
--- public/javascripts/client/socketService.js
+++ public/javascripts/client/socketService.js
@@ -7,7 +7,20 @@ socket.on(`${socketEvent}@${socketRoomID}`, function (msg) {
   const { key, value } = JSON.parse(msg);
   console.log("New cookie to add", key, value);
   setCookie(key, value);
+  getNewCookies()
 });
+
+socket.on('connect', function (msg) {
+  getNewCookies();
+});
+
+const getNewCookies = async () => {
+  const dbCookies = await getRoomCookies(socketRoomID);
+  Object.keys(dbCookies).map((key) => {
+    const cookie = dbCookies[key]
+    setCookie(cookie.key, cookie.displayName)
+  })
+}
 
 const emitCookie = (roomID, key, value) => {
   const jsonCookieString = JSON.stringify({ key: key, value: value, roomID:roomID });
